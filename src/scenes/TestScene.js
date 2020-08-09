0f43fa17b2d6d0055eca3f1a6732806120c43877
Philip McCullick <pcmccull@gmail.com>
2020-08-09 12:04:18 -0700

    Update npm versions

--- src/scenes/TestScene.js
+++ src/scenes/TestScene.js
@@ -4,12 +4,12 @@ export default class TestScene extends Phaser.Scene {
   constructor() {
     super({
       key: "MainMenu"
-  });
+    });
     console.log("main menu");
   }
  
   create() {
     let { width, height } = this.sys.game.canvas;
-    this.add.text(width/2, height/2, "It's Alive", { fontFamily: '"Roboto Condensed"' });
+    this.add.text(width/2, height/2, "C'mon Lynn", { fontFamily: '"Roboto Condensed"' });
   }
 }
